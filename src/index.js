3cb7bc947d1f1a215cc70a325f01bc56061818d1
Shorouq Ishtaiah <shorouq.ish99@gmail.com>
2022-09-27 12:53:12 +0300

    React pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter  } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import { Route, Routes} from 'react-router-dom';
 import Countries from "./Countries";
 
@@ -10,8 +10,10 @@ import Details from "./Details";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename='/'>
+  <React.StrictMode>
+  <BrowserRouter >
      <App/>
-  </HashRouter>
+  </BrowserRouter>
+  </React.StrictMode>
  
 );
